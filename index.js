ca05e50e6b2d7715670bb0c9dd2ce6e0313e8017
わたあめえ <Wataamee777@gmail.com>
2025-12-21 12:19:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import fs from "fs";
 import dotenv from "dotenv";
 import basicAuth from "./middleware/basicAuth.js";
+import products from "./products.json" assert { type: "json" };
 
 dotenv.config();
 
