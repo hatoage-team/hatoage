f31c16e99293053c84009fb3c52b6b8a11e1e828
わたあめえ <Wataamee777@gmail.com>
2026-01-31 16:31:25 +0900

    Create mailer.js

---
+++ mailer.js
@@ -0,0 +1,21 @@
+// mailer.js
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+});
+
+export async function sendMail({ to, subject, html }) {
+  await transporter.sendMail({
+    from: process.env.MAIL_FROM,
+    to,
+    subject,
+    html
+  });
+}
