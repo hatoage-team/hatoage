1ba59edd6e3950af3a32f8196e933a1e19083a0c
わたあめえ <Wataamee777@gmail.com>
2026-02-01 09:27:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.get("/api/products", cors(), (_, res) => {
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
